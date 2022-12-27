c51e3b4ff28add850b853da7cd62ab9e6f567d68
YuDeokRin <ejrfls12@naver.com>
2022-12-27 23:41:27 +0900

    예제 - JS에서 Freezing이란?

---
+++ src/main/java/me/chapter02/item02/freezing/FreezeTest.js
@@ -0,0 +1,22 @@
+var dante = {
+    'name': 'DeokRin', // property(프로퍼티)
+    'age': '30'
+};
+
+// const dante = {
+//     'name': 'DeokRin', // property(프로퍼티)
+//     'age': '30'
+// };
+
+
+dante = {
+    'name': 'Mando'
+}
+
+// delete dante.name;// undefined
+
+dante.pet = ["하늘다람쥐"]
+dante.pet.push("고양이");
+console.info(dante.name)
+
+console.info(dante.pet)
